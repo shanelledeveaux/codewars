425efa85e1dde7c709780dd5d06c665aa725d2bc
shanellewebster <webstershanelle@gmail.com>
2018-08-12 21:10:03 -0500

    6k

--- solutions.js
+++ solutions.js
@@ -188,3 +188,16 @@
 // var favNum = 7;
 // var trueFavNum = 7;
 // trueFavNum === favNum;
+
+// function solution(number) {
+//     var sum = 0;
+//     for (var i = 1; i < number; i++) {
+//       console.log(i)
+//       if (i % 3 === 0 || i % 5 === 0) {
+//         sum += i;
+//       }
+//     }
+//     return sum;
+//   }
+
+//   solution(10);
