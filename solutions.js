c16e3573c430a305df132ce0102983d48b9dbbc6
Shanelle Webster <webstershanelle@gmail.com>
2018-10-28 20:46:17 -0500

    fizz buzz

--- solutions.js
+++ solutions.js
@@ -243,3 +243,21 @@ function superSize(num) {
 }
 
 superSize(123456);
+
+function fizzbuzz(n) {
+  var arr = [];
+  for (var i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      arr.push("Fizz");
+    } else if (i % 5 === 0) {
+      arr.push("Buzz");
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+
+fizzbuzz(20);
