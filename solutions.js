05cbc35ceb333ab77c26f03f1f02ded3df3e52ae
Shanelle Webster <webstershanelle@gmail.com>
2018-10-31 09:35:36 -0500

    bubble sort

--- solutions.js
+++ solutions.js
@@ -281,3 +281,20 @@ function rakeGarden(garden) {
   }
   return rakedGarden.join(" ");
 }
+
+function swap(array, i, j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function bubblesortOnce(array) {
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 1; j < array.length; j++) {
+      if (array[j - 1] > array[j]) {
+        swap(array, j - 1, j);
+      }
+    }
+  }
+  return array;
+}
